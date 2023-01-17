028554399fd52aecbb504a53894d95bf06452d5e
Halima Akter Ratna <122334425+Halima258@users.noreply.github.com>
2023-01-17 09:31:16 +0600

    Add files via upload

---
+++ Lab-01/Codforce1.cpp
@@ -0,0 +1,24 @@
+//Codforce problem no-4A
+//Problem name-Watermelonn
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter the weight of watermelon";
+    cin>>x;
+    if (1<=x && x<=100){
+        if (x%2==0){
+            cout<<"Yes"<<end1;
+
+        }
+        else
+        {
+            Cout<<"No"<<end1;
+        }
+    }
+  else
+    cout<<"wrong weight";
+  return 0;
+
+}
